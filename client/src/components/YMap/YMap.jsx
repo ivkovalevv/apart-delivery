89be2bd2f349b4b688d7ca3cfb292fbe3280e18c
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-24 10:32:18 +0000

    Добавил google maps через iframe

--- client/src/components/YMap/YMap.jsx
+++ client/src/components/YMap/YMap.jsx
@@ -3,12 +3,15 @@ import "./ymap.css";
 
 const Ymap = () => {
   return (
-    <YMaps>
-      <Map
-        defaultState={{ center: [59.93818, 30.365644], zoom: 12 }}
-        className="componentMap"
-      />
-    </YMaps>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d274450.6497260677!2d11.596580418800349!3d48.134840853450434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sde!4v1603744450305!5m2!1sen!2sde"
+        width="600"
+        height="450"
+        frameborder="0"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
   )
 }
 
