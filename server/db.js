3c9833ddd101bd3669dc5fb9dc272b1a696ff368
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-08-04 16:10:44 +0300

    add schema public db.js

--- server/db.js
+++ server/db.js
@@ -5,6 +5,7 @@ module.exports = new Sequelize(
     process.env.DB_USER, // Пользователь
     process.env.DB_PASSWORD, // Пароль
     {
+        schema: 'public',
         dialect: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
