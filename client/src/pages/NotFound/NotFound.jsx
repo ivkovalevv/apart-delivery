fd62a8be9336fb79db993a525484992d5fd0a35d
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-08-25 11:00:08 +0000

    Стилизована страница 404

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./notfound.css";
 
 const NotFound = () => {
-  return <div>404 NOT FOUND</div>;
+  return (
+    <div className="notfound-wrapper">
+      <h1 className="notfound-heading">404</h1>
+      <h2 className="notfound-description">
+        К сожалению, такой странцы не существует :(
+      </h2>
+    </div>
+  );
 };
 
 export default NotFound;
